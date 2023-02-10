a37698b82eceb3807341516dd855ba25872265dd
yoonstar1996 <yagobo1110@naver.com>
2023-02-10 16:55:07 +0900

    section2.32

---
+++ react-review/src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="React Review" />
+    </>
+  );
+}
+
+export default App;
